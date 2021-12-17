a72c47804f80ac0f870bf746866e52decbe3b0ac
chunche95 <37621958+chunche95@users.noreply.github.com>
2021-12-17 17:00:37 +0100

    Ficheros de proyecto de Cine Online UCLM

---
+++ CineOnline/src/main/java/humancomputing/net/cineonline/CineOnline.java
@@ -0,0 +1,53 @@
+/**
+ * CINE ONLINE.
+ * 
+ * Un cine online permite almacenar las películas de estreno y las opciones de M críticos de cine
+ * en forma de estrellas, siendo 1 estrella la puntuación mínima y 5 la puntuación máxima. 
+ * Para facilitar la tarea del jurado, se desea elaborar un programa que se encargue de gestionar toda
+ * la información, contabilizando los votos emitidos y obteniendo el ganador. Así pues, dicho
+ * programa consistirá en:
+ *  • Deberá preguntar qué tipo de rol tenemos, si administrador, crítico de cine o usuario.
+ *  • En caso de que seamos administrador:
+ *      o Leer del teclado el número N de películas que tiene el cine
+ *      o Leer del teclado los títulos de las N películas
+ *      o Leer del teclado el número M de críticos
+ *      o Leer del teclado el nombre de los M críticos
+ *      o Una vez que hayamos escrito el nombre de todos los críticos, crearemos un fichero por 
+ *        cada crítico y un único fichero que contenga el nombre de las películas. 
+ *        Si fuese necesario, se puede crear un fichero que contenga todos los críticos.
+ *  • En caso de que seamos críticos:
+ *      o El sistema nos preguntará nuestro nombre y lo buscará en el sistema, donde el administrador 
+ *        lo habrá escrito previamente
+ *      o Si el nombre se encuentra en el fichero, le mostrará el nombre de cada película y le pedirá 
+ *        la puntuación de cada una (del 1 al 5)    
+ *      o Una vez hayamos terminado, guardará en el fichero correspondiente del crítico, 
+ *        las calificaciones de cada película junto a su nombre. 
+ *              Por ejemplo, el fichero Fernando.txt tendrá:
+ *                      Máquina de Turing - 4
+ *                      Scarface – 5
+ *  • En caso de que seamos usuarios, nos mostrará un menú que conste de las siguientes
+ *  opciones:
+ *      o Mostrar película con menor media
+ *      o Mostrar película con mayor media
+ *      o Mostrar el crítico que menor puntuación media ha dado a todas las películas (Si hay varias, 
+ *        mostrar solo una de ellas)
+ *      o Mostrar las películas con más de 3 de media
+ *      o Finalizar el programa
+ *  • Al finalizar el programa, guardar en un fichero de texto los nombres de las películas y las
+ * puntuaciones media asociadas. Para ello, se considerará que en una línea aparecerá el
+ * nombre de la película, y en la siguiente línea la puntuación media.
+ * 
+ */
+package humancomputing.net.cineonline;
+
+/**
+ * @version 0.1
+ * @author Paulino Esteban Bermúdez R.
+ */
+public class CineOnline {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al CINE ONLINE.");
+        // Métodos del cine.
+    }
+    
+}
