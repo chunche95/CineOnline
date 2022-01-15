84273fc899e161a3c4f298474942cf159c2b12f4
chunche95 <37621958+chunche95@users.noreply.github.com>
2022-01-15 11:44:34 +0100

    criticos y usuarios modificado

--- CineOnline/CineCriticos/src/net/humancomputing/cineonline20220104/AltasCRITICOS.java
+++ CineOnline/CineCriticos/src/net/humancomputing/cineonline20220104/AltasCRITICOS.java
@@ -15,8 +15,8 @@ import java.util.Scanner;
  * 	+---> NO EXISTE! --> Sacar mensaje que no existe.
  * 	+---> EXISTE!
  * 		+ Mostrar nombres de las peliculas. (Lectura de fichero)
- * 		+ Pedir puntuación c/u peliculas del 1-5
- * 		+ Guardar puntuación c/u peliculas.
+ * 		+ Pedir puntuaciï¿½n c/u peliculas del 1-5
+ * 		+ Guardar puntuaciï¿½n c/u peliculas.
  *
  * @version 0.1
  * @author Luis Miguel Barreiro Cabello.
@@ -25,69 +25,63 @@ import java.util.Scanner;
 /**
  * RECOMENDACIONES PARA EL DESARROLLO DEL APARTADO.
  * 
- * - Debes preguntar el nombre del critico, para que se escriba.
- * - Debes crear un método o algo que te busque la existencia del nombre del critico que se escribe por teclado.
+ * (HECHO!) Debes preguntar el nombre del critico, para que se escriba.
+ * (HECHO!) Debes crear un mÃ©todo o algo que te busque la existencia del nombre del critico que se escribe por teclado.
  * 
- * - El fichero no puede llamarse Juan.txt, puede haber varias Juanes entre los criticos y el sistema fallaria. 
+ * (HECHO! - 'nombre'.txt) El fichero no puede llamarse Juan.txt, puede haber varias Juanes entre los criticos y el sistema fallaria.
  * 
- * - Para el control de errores, debes evitar que escriba una letra distinta a 's', si escribo 'S', sale pero no me indica nada el programa.
- * si escribo '23' o 'f', no me dice que es invalido.
+ * (PENDIENTE!) - Tras encontrar el crÃ­tico, debes mostrarle las peliculas que el administrador dio de alta en el cine
+ * (PENDIENTE!) - Debes pedirle una nota del 1 al 5 por cada pelicula que le muestras.  
  * 
- * - Sería una buena practica ir dejando algún comentario más, por ejemplo para el tema de los ficheros, de que trata cada uno.
- * - También, podrías indicar la finalización del programa con un mensaje, o un mensaje y volviendo al menu principal.	  
+ *
+ * - TambiÃ©n, podrÃ­as indicar la finalizaciÃ³n del programa con un mensaje, o un mensaje y volviendo al menu principal.	  
  *  
  * (Adjunto ficheros para hacer las pruebas)
  * @author pauli
  *
  */
 public class AltasCRITICOS {
-
-	public static void main(String[] args)throws IOException {
-		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
-		File MiFichero;
-		PrintWriter salida;
-		String Titulo="Juan.txt";
-		MiFichero=new File(Titulo);	
-		salida=new PrintWriter(new FileWriter(MiFichero));
-		boolean Escribirmas=true;
-		do {
-			System.out.println("Introduce nombre de pelicula ");
-			int resultado=generarNumero();
-			
-			
-			String nombre=sc.next();
-			salida.println(nombre+" - "+resultado);// syso en la consola sin esto no se reproduce el fichero 
-			
-			System.out.println("Introduce mas nombres s/n");
-			if(sc.next().equals("s")){
-				Escribirmas=true;
+	public static boolean existeCritico(String nombre) throws FileNotFoundException {		
+		Scanner sc = new Scanner(new File("criticos.txt"));
+		// ver si hay linea en el fichero por eso usamos el sc.hasNextLine
+		while(sc.hasNextLine()) {
+			String linea = sc.nextLine();
+			if (linea.equals(nombre)) {
+				sc.close();
+				return true;
 			}
-			else {
-				Escribirmas=false;
-			}
-		} while (Escribirmas);
-	
-		salida.close();
-		// Este array almacenara mas adelante los alaias y las puntuaciones por separado
-		
-		
+		}		
 		sc.close();
-		
-			
-			
-				
-			}
-			
-			
-		
-		
-	
-	
-	public static int generarNumero() {
+		return false;
+	} // Cierre del mÃ©todo existeCritico	
+	public static void main(String[] args) throws IOException {		
+		// 1 Preguntar por el critico al usuario
+		Scanner sc = new Scanner(System.in);
+		boolean existeCritico = false;
+		do {
+			System.out.print("Â¿Nombre del critico? ");
+			String nombre = sc.nextLine();			
+			// 2 Buscar el critico en la lista
+			existeCritico = existeCritico(nombre);
+			if (existeCritico) {
+				puntuarPeliculas(nombre);
+			} else {
+				System.out.println("El critico no esta dado de alta");
+			}			
+		} while(!existeCritico);		
+	}	
+	public static void puntuarPeliculas(String nombre) throws IOException {
+		// crear fichero critico  nombre.txt		
+		File critico = new File(nombre + ".txt");
+		FileWriter fw = new FileWriter(critico);		
+		// Leer fichero de peliculas
+		// Para cada pelicula, pedir puntuacion al usuario
+		// Escribir en el fichero del critico, la linea con pelicula + puntuacion		
+		int valor=GenerarNumero();	
+	}		
+	public static int GenerarNumero() {
 		int numero;
-		numero= (int)(Math.random()*5-1);
+		numero=(int)(Math.random()*5-1);
 		return numero;
-	}
-		
+	}		
 }
--- CineOnline/CineUsuarios/src/net/humancomputing/cineonline20220104/Usuarios.java
+++ CineOnline/CineUsuarios/src/net/humancomputing/cineonline20220104/Usuarios.java
@@ -8,131 +8,92 @@ import java.util.Scanner;
  * 
  * + Mostrar peliculas con menor media
  * + Mostrar peliculas con mayor media
- * + Mostrar pelÌculas con +3 estrellas.
+ * + Mostrar pel√≠culas con +3 estrellas.
  * + Mostrar criticos que han dado menor puntuacion a todas las peliculas.
  *
  * @version 0.1
- * @author EfraÌn Enrique RamÌrez Mojica.
+ * @author Efra√≠n Enrique Ram√≠rez Mojica.
  */
-
+ /** 
+ * USUARIOS DE CINES MKE.
+ * 
+ * Tareas de usuarios:
+ * 
+ * + Mostrar peliculas con menor media
+ * + Mostrar peliculas con mayor media
+ * + Mostrar peliculas con +3 estrellas.
+ * + Mostrar criticos que han dado menor puntuacion a todas las peliculas.
+ *
+ * @version 0.1
+ * @author Efrain Enrique Ramirez Mojica.
+ */
 /**
  * RECOMENDACIONES PARA EL DESARROLLO DEL APARTADO.
+ * (HECHO!) - Men√∫ de opciones
+ *
+ * (PENDIENTE!) - Desarrollar los m√©todos de cada opci√≥n.
  * 
- * - Si lo haces a travÈs de un menu seria m·s sencillo crear tu parte, por ejemplo:
- * +----------------------- MENU -----------------------+
- * + 			Seleccione una opcion:					+
- * +													+
- * +	 		1) Mostrar peliculas con menor media	+
- * + 			2) Mostrar peliculas con mayor media	+
- * + 			3) Mostrar pelÌculas con +3 estrellas.	+
- * + 			4) Mostrar criticos que han dado menor puntuacion a todas las peliculas.Dar de alta peliculas.
- * +			5) Volver al menu principal o salir.	+
- * +-------------------- FIN MENU ----------------------+
- * 
- * - Debes probar que cada opciÛn funcione, que no entren en bucle infinito y que al darle a salir, el programa finalice sin errores.
- * 
- * - Para ver las peliculas de menor y mayor puntuacion, debes leer los ficheros de puntuacion de los criticos e ir almacenando la puntuacion de 1 al 5 que le dieron a la pelicula y luego 
+ * 1 y 2) Para ver las peliculas de menor y mayor puntuacion, debes leer los ficheros de puntuacion de los criticos e ir almacenando la puntuacion de 1 al 5 que le dieron a la pelicula y luego 
  * mostrar la que tiene menos y mas nota segun sea (opcion 1 o 2 del menu)
  * 
- * - Para ver las peliculas de +3 puntos. Debes crear un sitio donde vayas leyendo la pelicula y la puntuacion, el que sea +3, lo guardas, en caso contrario, lo saltas.
+ * 3) Para ver las peliculas de +3 puntos. Debes crear un sitio donde vayas leyendo la pelicula y la puntuacion, el que sea +3, lo guardas, en caso contrario, lo saltas.
  * 
- * - Para ver los criticos con menor puntuacion, debes sumar la nota del fichero de cada critico y hacer su media, la que de menos, se muestra el nombre.
+ * 4) Para ver los criticos con menor puntuacion, debes sumar la nota del fichero de cada critico y hacer su media, la que de menos, se muestra el nombre.
  * 
  * (Adjunto ficheros para hacer las pruebas)
  * @author pauli
  *
  */
 public class Usuarios {
-
 	public static void main(String[] args) {
-		//DeclaraciÛn del objeto que nos permite la entrada de datos.
+		//Declaraci√≥n del objeto que nos permite la entrada de datos.
 		Scanner leer = new Scanner(System.in);
-		
+		//Menu de opciones
 		boolean salir = false;
 		int opcion;
 		
 		while (! salir) {
-			//Opciones de las peliculas que hay para escoger
-			System.out.println(" 1. Opcion 1 ");
-			System.out.println(" 2. Opcion 2 ");
-			System.out.println(" 3. Opcion 3 ");
-			System.out.println(" 4. Opcion 4 ");
-			System.out.println(" 5. Opcion 5 ");
-			System.out.println(" 6. Opcion 6 ");
-			System.out.println(" 7. Salir ");
-			
+			System.out.println("\t\t Men√∫ principal - USUARIOS.");
+			System.out.println("");
+			System.out.println("Seleccione una de las siguientes opciones:\n");
+			System.out.println("\t 1. Mostrar peliculas con menor media ");
+			System.out.println("\t 2. Mostrar peliculas con mayor media ");
+			System.out.println("\t 3. Mostrar el critico que menor puntuacion media ha dado a todas las peliculas(Si hay varias mostrar solo una de ellas)");
+			System.out.println("\t 4. Mostrar peliculas con +3 de media ");
+			System.out.println("");
+			System.out.println("\t 0. Salir del programa");
 			try {
 				//Solicitamos que el usuario escoga una opcion y lo recibimos desde el teclado para guardarlo en la variable correspondiente
-			System.out.println(" Introduce un numero: ");
-			opcion = leer.nextInt();
-			
-			switch (opcion) {
-			
-			case 1:
-				System.out.println(" Esta es la opcion 1");
-				break;
+				System.out.println("\n Introduce un numero: ");
+				opcion = leer.nextInt();
 				
-            case 2:
-            	System.out.println(" Esta es la opcion 2");
-				break;
+				switch (opcion) {					
+					case 1:
+						System.out.println(" Esta es la opcion mostrar peliculas con menor media ");
+						break;					
+					case 2:
+		            	System.out.println(" Esta es la opcion mostrar peliculas con mayor media ");
+						break;					
+		            case 3:
+		            	System.out.println(" Esta es la opcion Mostrar el critico que menor puntuacion media ha dado a todas las peliculas(Si hay varias mostrar solo una de ellas) ");
+						break;					
+		            case 4:
+		            	System.out.println(" Esta es la opcion mostrar peliculas con +3 de media ");
+						break;					
+		            case 0:
+		            	salir = true;
+		 				break;
+		            default:
+		            	System.out.println(" Las opciones son entre 1 y 4 ");
+		            	System.out.println(" Pulse '0' para salir del programa.");
+	 			} //Fin del Switch
 				
-            case 3:
-            	System.out.println(" Esta es la opcion 3");
-				break;
-				
-            case 4:
-            	System.out.println(" Esta es la opcion 4");
-				break;
-				
-             case 5:
-            	 System.out.println(" Esta es la opcion 5");
-				break;
-				
-             case 6:
-            	 System.out.println(" Esta es la opcion 6");
- 				break;
- 				
-             case 7:
-            	salir = true;
- 				break;
- 				default:
- 			System.out.println(" Las opciones son entre 1 y 7 ");
- 				
- 				
-				
-				
-				
-				
-				
-				
-		} // fin Switch
-		} catch (AnnotationTypeMismatchException e ) {            //Utilizamos un try y catch para que de error si no se escibe un numero y diga que debe ingresar un numero
-				
-		System.out.println(" Debes introducir un numero: ");
-		leer.next();
-		}
-          System.out.println(" Fin del menu ");
-    }// fin while
-		System.out.println(" pelicula con mayor puntuacion y menor puntuacion ");
-		System.out.println(" Digite la cantidad de peliculas a evaluar: ");
-		int cantidaddepeliculas = leer.nextInt();
-		int peliculaconmenorpuntuacion = 0;
-		int peliculaconmayorpuntuacion = 0;
-		
-		for(int i = 0; i < cantidaddepeliculas; i++) {
-		System.out.println(" Digite la pelicula en la posicion: " + (i + 1));
-		int pelicula = leer.nextInt();
-		if (i == 0) {
-			peliculaconmenorpuntuacion = pelicula;
-			peliculaconmayorpuntuacion = pelicula;
-		} else if (pelicula < peliculaconmenorpuntuacion) { 
-			peliculaconmenorpuntuacion = pelicula;
-			} //else if
-		if(pelicula > peliculaconmayorpuntuacion) {
-		   peliculaconmayorpuntuacion = pelicula; 
-			} //if
-		} //for
-		System.out.println(" la pelicula con menor puntuacion es: " + peliculaconmenorpuntuacion);
-		System.out.println(" la pelicula con mayor puntuacion es: " + peliculaconmayorpuntuacion);
-	}
-}
+			} catch (AnnotationTypeMismatchException e ) {  //Utilizamos un try y catch para que de error si no se escibe un numero y diga que debe ingresar un numero
+				System.out.println("+_________________________________________+");
+				System.out.println("| Debes introducir un numero entre 0 y 4! |");
+				System.out.println("+_________________________________________+");
+				leer.next();
+			} //Fin del Catch			
+		}//Fin del While
+	} // Cierre del Main
+} // Cierre de la clase 
